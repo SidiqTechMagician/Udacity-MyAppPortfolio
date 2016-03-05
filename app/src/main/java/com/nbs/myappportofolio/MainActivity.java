76d7196c7c9583a058d9f6202ce2bc6661c61afa
sidiq permana <permana.sidiq@gmail.com>
2016-03-05 15:54:08 +0700

    Add listener that i forgot to put in this early morning

--- app/src/main/java/com/nbs/myappportofolio/MainActivity.java
+++ app/src/main/java/com/nbs/myappportofolio/MainActivity.java
@@ -3,14 +3,31 @@ package com.nbs.myappportofolio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnSpofity, btnScoresApp, btnLibraryApp, btnBuildBigger,
+    btnXyzReader, btnCapstone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSpofity = (Button)findViewById(R.id.btn_spotify);
+        btnScoresApp = (Button)findViewById(R.id.btn_scores_app);
+        btnLibraryApp = (Button)findViewById(R.id.btn_library_app);
+        btnBuildBigger = (Button)findViewById(R.id.btn_build_bigger);
+        btnXyzReader = (Button)findViewById(R.id.btn_xyz_reader);
+        btnCapstone = (Button)findViewById(R.id.btn_capstone);
+
+        btnSpofity.setOnClickListener(this);
+        btnScoresApp.setOnClickListener(this);
+        btnLibraryApp.setOnClickListener(this);
+        btnBuildBigger.setOnClickListener(this);
+        btnXyzReader.setOnClickListener(this);
+        btnCapstone.setOnClickListener(this);
     }
 
     @Override
